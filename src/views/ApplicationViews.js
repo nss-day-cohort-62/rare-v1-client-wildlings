0428f5acbdede3cf0640cf31e1179f09a6b39b79
Jackson Henderson <jbhhenderson@gmail.com>
2023-04-24 14:05:27 -0500

    added singular post logic

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -3,6 +3,7 @@ import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
 import { Authorized } from "./Authorized"
 import { PostsList } from "../components/posts/PostsList"
+import { Post } from "../components/posts/Post"
 
 export const ApplicationViews = ({ token, setToken }) => {
   return <>
@@ -10,7 +11,8 @@ export const ApplicationViews = ({ token, setToken }) => {
       <Route path="/login" element={<Login setToken={setToken} />}  />
       <Route path="/register" element={<Register setToken={setToken} />}  />
       <Route element={<Authorized token={token} />} >
-        <Route path="/posts" element={<PostsList />} />        
+        <Route path="/posts" element={<PostsList />} /> 
+        <Route path="/posts/:post_id" element={<Post />} /> 
       </Route>
     </Routes>
   </>
