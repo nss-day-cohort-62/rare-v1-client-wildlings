bd6838f5d6b7cfce38fb6fc857ba228488dfa3f2
Freya Potempa <FreyaPotempa@gmail.com>
2023-04-25 15:10:43 -0500

    posts created and navigated to

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -1,21 +1,25 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import { getSinglePost } from "../../managers/PostsManager"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getSinglePost } from "../../managers/PostsManager";
+import { HumanDate } from "../utils/HumanDate";
 
 export const Post = () => {
-    const [post, setPost] = useState({})
-    const {post_id} = useParams()
+  const [post, setPost] = useState({});
+  const { post_id } = useParams();
 
-    useEffect(() => {
-        getSinglePost(post_id)
-        .then((data) => setPost(data))
-    },[])
+  useEffect(() => {
+    getSinglePost(post_id).then((data) => setPost(data));
+  }, []);
 
-    return <>
-        <h1>{post.title}</h1>
-        <div>{post?.user?.first_name} {post?.user?.last_name}</div>
-        <div>{post?.category?.label}</div>
-        <div>{post.publication_date}</div>
-        <div>{post.content}</div>
+  return (
+    <>
+      <h1>{post.title}</h1>
+      <div>
+        {post?.user?.first_name} {post?.user?.last_name}
+      </div>
+      <div>{post?.category?.label}</div>
+      <div>{post.publication_date}</div>
+      <div>{post.content}</div>
     </>
-}
+  );
+};
--- src/managers/PostsManager.js
+++ src/managers/PostsManager.js
@@ -1,9 +1,19 @@
 export const getAllPosts = () => {
-    return fetch('http://localhost:8088/posts')
-        .then(res => res.json())
-}
+  return fetch("http://localhost:8088/posts").then((res) => res.json());
+};
 
 export const getSinglePost = (post_id) => {
-    return fetch(`http://localhost:8088/posts/${post_id}`)
-        .then(res => res.json())
-}
+  return fetch(`http://localhost:8088/posts/${post_id}`).then((res) =>
+    res.json()
+  );
+};
+
+export const createPost = (newPost) => {
+  return fetch(`http://localhost:8088/posts`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newPost),
+  }).then((res) => res.json());
+};
