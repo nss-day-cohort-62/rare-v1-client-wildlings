f799fa10fea4881bb65e24c0dc3c9cc671446fc1
Freya Potempa <FreyaPotempa@gmail.com>
2023-05-22 11:02:44 -0500

    posts viewable

--- src/managers/PostsManager.js
+++ src/managers/PostsManager.js
@@ -1,23 +1,31 @@
-const url = "http://localhost:8000"
+const url = "http://localhost:8000";
 
 export const getAllPosts = () => {
-  return fetch(`${url}/posts`).then((res) => res.json());
+  return fetch(`${url}/posts`, {
+    headers: {
+      Authorization: `Token ${localStorage.getItem("auth_token")}`,
+    },
+  }).then((response) => response.json());
 };
 
 export const getSinglePost = (post_id) => {
-  return fetch(`${url}/posts/${post_id}`).then((res) =>
-    res.json()
-  );
+  return fetch(`${url}/posts/${post_id}`, {
+    headers: {
+      Authorization: `Token ${localStorage.getItem("auth_token")}`,
+    },
+  }).then((response) => response.json());
 };
 
 export const getMyPosts = (userId) => {
-  return fetch(`${url}/posts/?_user=${userId}`).then((res) =>
-    res.json()
-  );
+  return fetch(`${url}/posts/?_user=${userId}`, {
+    headers: {
+      Authorization: `Token ${localStorage.getItem("auth_token")}`,
+    },
+  }).then((response) => response.json());
 };
 
 export const createPost = (newPost) => {
-  return fetch(`http://localhost:8088/posts`, {
+  return fetch(`http://localhost:8000/posts`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -27,17 +35,17 @@ export const createPost = (newPost) => {
 };
 
 export const deletePost = (postId) => {
-  return fetch(`http://localhost:8088/posts/${postId}`, {
+  return fetch(`http://localhost:8000/posts/${postId}`, {
     method: "DELETE",
-  })
-}
+  });
+};
 
 export const updatePost = (postObj) => {
-  return fetch(`http://localhost:8088/posts/${postObj.id}`, {
+  return fetch(`http://localhost:8000/posts/${postObj.id}`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(postObj),
-  })
+  });
 };
